20b6b5e77d061cc4f9aeeedf861132b24f9016a6
Jdelaney1991 <delaneyep91@gmail.com>
2023-07-20 03:59:25 +0000

    new code
    Signed-off-by: Jdelaney1991 <delaneyep91@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include "mbed.h"
 
 // main() runs in its own thread in the OS
+
+// 0, 1, 1, 2, 3, 5, 8, 13
+
 int main()
 {
-    while (true) {
+    printf("Lab 2 program \r\n");
+    int max_term = 10;
+    int term_1 = 0;
+    int term_2 = 1;
+    int next_term = 0;
 
+    for (int i = 1; i <= max_term; i++) {
+        if (i == 1)
+            printf("%d, ", term_1);
+        else if (i == 2)
+            printf("%d, ", term_2);
+        else {
+            // 3rd term = previous terms sum
+            next_term = term_1 + term_2;
+            term_1 = term_2;
+            term_2 = next_term;
+            printf("%d, ", next_term);
+        }
     }
+
+    printf("\r\n");
+  
+      while (true) { } // Added closing curly brace for the while loop
 }
 
